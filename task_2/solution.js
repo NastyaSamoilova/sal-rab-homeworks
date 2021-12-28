5db8f2c2582a269e4941a2643477fce1c61fd4f9
Nastya Samoilova <samoilova_35@mail.ru>
2021-12-28 18:50:41 +0300

    task 2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -6,7 +6,7 @@ function calcShipping(sum, min, shipping) {
     // Задание №2.1. Рассчитать доставку
 
     let shippingSum;
-    if (productsSum == 0 ❘❘ productsSum >= freeShippingMinSum) {
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {
         shippingSum = 0;
     } else {
         shippingSum = shippingPrice;

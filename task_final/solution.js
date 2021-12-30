deade32440ce303d08a9b8560c3bdee3f194ff92
Nastya Samoilova <samoilova_35@mail.ru>
2021-12-30 13:04:44 +0300

    final task done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,8 +34,12 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let data = {
+        client: {name, phone}, 
+        order: {address{street, house, entrance, floor, flat}, sum},
+        goods: {count, title}, 
+    }
+}
     let countOfGoods = goods.length;
 
     for (let i = 0; i <= countOfGoods; i += 1) {
